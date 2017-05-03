b4b49b1e22c11f4a4d292537b3887abbd001fe66
Jose Alvaro Garcia <jag040393@gmail.com>
2017-05-02 22:51:03 -0700

    Tried but failed

--- gatickets.js
+++ gatickets.js
@@ -5,28 +5,56 @@ const Nightmare = require('nightmare');
 const async = require('async');
 	nightmare = Nightmare({show:true});
 
+
+
+
+let goToScrapedLinks = (data)=>{	
+ 	let goToUrl = (url)=>{
+ 	  nightmare
+		.goto(url)
+  		.wait(2000)
+  		.evaluate((err) =>{
+   			 if (err) {
+      			console.log(err);
+    		}
+    		 console.log('Done with ' + url);
+   }).then(()=>{
+   	console.log('working');
+   }) 
+ }
+  async.eachSeries(data,goToUrl,(err,res)=>{
+  	if(err) throw (err);
+  })
+}
+
+let scrapeAllEventLinks = (cb)=>{
+
 nightmare
 	.goto('https://www.draisbeachclub.com/calendar/')
-	.wait(5000)
 	.evaluate(()=>{
-		var results = [];
+		let results = [];
 		 $('.uvc-overevent').each(function() {
         		var dataId = $(this).attr('data-id');
         		var dataDate = $(this).attr('data-date');
         		results.push('http://www.draisbeachclub.com/uvtix/index.html?id='+dataId+'&date='+dataDate);
-
          });
-        return results;
+         	return results
 	})
 	.end()
 	.then((data)=>{
-	    async.forEachLimit(data,1,function(data){
-				nightmare
-				.goto(data)
-				.wait(5000)
-				.end()
-			})
-	})
+		cb(data);
+		})
+}
+
+	
+
+
+scrapeAllEventLinks(goToScrapedLinks);
+
+
+
+
+
 
 
 
