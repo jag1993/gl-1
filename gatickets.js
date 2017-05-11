43611b362d849228dc4cfa4f2b1ec88cbe7ed653
Jose Alvaro Garcia <jag040393@gmail.com>
2017-05-10 17:44:53 -0700

    scraping links works but is unfiltered

--- gatickets.js
+++ gatickets.js
@@ -4,58 +4,69 @@ const request = require('request');
 const Nightmare = require('nightmare');
 const async = require('async');
 	nightmare = Nightmare({show:true});
+let webDriver;
 
 
+function initWebDriver(callback) {
+  webDriver = Nightmare({
+    show: true,
+    openDevTools: true
+  });
+
+  callback(null);
+}
+
+function scrapeTicketUrlData(callback){
+  webDriver
+    .viewport(1000,1000)
+    .goto('http://www.taolasvegas.com/nightclub/')
+    .evaluate(function(){
+      //var arrayOfLinks = document.querySelectorAll('a.tickets');
+      var arrayOfLinks = document.querySelectorAll('.owl-stage .owl-item .list-view-item a');
+      var arrayOfLinksHREF = [];
+      
+      arrayOfLinks.forEach(function(data){
+        arrayOfLinksHREF.push(data.href);
+      })
+
+      return arrayOfLinksHREF;
+    })
+    .then(function(arrayOfLinksHREF){
+      if (arrayOfLinksHREF != null && arrayOfLinksHREF.length > 0) {
+        console.log('----SCRAPED LINKS----');
+        callback(null, arrayOfLinksHREF);
+      } else {
+        callback('FAILED at scrapeTicketUrlData');
+      }
+    })
+}
 
 
-let goToScrapedLinks = (data)=>{	
- 	let goToUrl = (url)=>{
- 	  nightmare
-		.goto(url)
-  		.wait(2000)
-  		.evaluate((err) =>{
-   			 if (err) {
-      			console.log(err);
-    		}
-    		 console.log('Done with ' + url);
-   }).then(()=>{
-   	console.log('working');
-   }) 
- }
-  async.eachSeries(data,goToUrl,(err,res)=>{
-  	if(err) throw (err);
+function scrapeTicketPricingData(arrayOfLinksHREF,callback){
+  //console.log(arrayOfLinksHREF);
+  //document.querySelector('.product-0 .quantity p .plus') <---SELECT PLUS  
+    function pageToPage(url,callback){
+      console.log(`${url} PAGE TO PAGE FUNC`);
+      callback(null);
+    }
+
+    async.eachSeries(arrayOfLinksHREF,pageToPage,function(){
+      console.log('done');
+    })
+}
+
+function run(){
+async.waterfall([initWebDriver,scrapeTicketUrlData,scrapeTicketPricingData],
+  function (err, arrayOfLinksHREF) {
+  webDriver.end(() => arrayOfLinksHREF).then(function (arrayOfLinksHREF) {
+    if(err){
+        console.log(err)
+    }else{
+        console.log("FINISHED~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        console.log(arrayOfLinksHREF);
+      }
+      })
   })
 }
 
-let scrapeAllEventLinks = (cb)=>{
-
-nightmare
-	.goto('https://www.draisbeachclub.com/calendar/')
-	.evaluate(()=>{
-		let results = [];
-		 $('.uvc-overevent').each(function() {
-        		var dataId = $(this).attr('data-id');
-        		var dataDate = $(this).attr('data-date');
-        		results.push('http://www.draisbeachclub.com/uvtix/index.html?id='+dataId+'&date='+dataDate);
-         });
-         	return results
-	})
-	.end()
-	.then((data)=>{
-		cb(data);
-		})
-}
-
-	
-
-
-scrapeAllEventLinks(goToScrapedLinks);
-
-
-
-
-
-
-
-
-
+run();
